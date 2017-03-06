b059c9374a88a3710ccf96ab89b20b70960aa1da
Held Grijo <shadyueh@gmail.com>
2017-03-06 03:11:20 -0400

    Mongoid setup

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,18 @@ module Raceday
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+     
+    #bootstraps mongoid within applications -- like rails console
+
+    Mongoid.load!('./config/mongoid.yml')
+
+    #which default ORM are we using with scaffold
+    #add --orm mongoid, or active_record
+    # to rails generate cmd line to be specific
+
+    #config.generators {|g| g.orm :active_record}
+
+    config.generators {|g| g.orm :mongoid}
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
