9260ec9ad91a6c79f16146d87726b910559d0dde
Held Grijo <shadyueh@gmail.com>
2017-03-06 04:11:45 -0400

    CRUD req 01

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -9,4 +9,14 @@ class Racer
   def self.collection
    self.mongo_client['racers']
   end
+
+  def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
+  	result = self.collection
+  		.find(prototype)
+  		.sort(sort)
+  		.skip(skip)
+  	result=result.limit(limit) if !limit.nil?
+  	return result
+  end
+
 end
