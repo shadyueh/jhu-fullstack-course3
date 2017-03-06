4fd9cb6ba561d1e4f7fa464d0374c63f2540482c
Held Grijo <shadyueh@gmail.com>
2017-03-06 03:46:10 -0400

    Database connection from model class

---
+++ app/models/racer.rb
@@ -0,0 +1,12 @@
+class Racer
+  include ActiveModel::Model
+  # convenience method for access to client in console
+  def self.mongo_client
+   Mongoid::Clients.default
+  end
+
+  # convenience method for access to zips collection
+  def self.collection
+   self.mongo_client['racers']
+  end
+end
